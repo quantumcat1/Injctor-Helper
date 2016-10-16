346e3a88973a1d223da58885f5eaf8cc9536767a
Erin <thequantumcat@hotmail.com>
2016-10-16 20:43:27 +1100

    Fixes formatting

--- src/main/Injector.java
+++ src/main/Injector.java
@@ -10,59 +10,59 @@ import java.util.concurrent.TimeUnit;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-
-
 public class Injector
 {
-	private static void copyFile (String pathIn, String pathOut) throws IOException
-	{
-	    File infile = new File(pathIn);
-	    File outfile = new File(pathOut);
+    private static void copyFile(String pathIn, String pathOut) throws IOException
+    {
+        File infile = new File(pathIn);
+        File outfile = new File(pathOut);
 
-	    FileInputStream instream = new FileInputStream(infile);
-	    FileOutputStream outstream = new FileOutputStream(outfile);
+        FileInputStream instream = new FileInputStream(infile);
+        FileOutputStream outstream = new FileOutputStream(outfile);
 
-    	byte[] buffer = new byte[1024];
+        byte[] buffer = new byte[1024];
 
-    	int length;
+        int length;
 
-	    while ((length = instream.read(buffer)) > 0)
-	    {
-	    	outstream.write(buffer, 0, length);
-	    }
+        while ((length = instream.read(buffer)) > 0)
+        {
+            outstream.write(buffer, 0, length);
+        }
 
-	    instream.close();
-	    outstream.close();
-	}
+        instream.close();
+        outstream.close();
+    }
 
-	private void extractZip(String zip) throws IOException
-	{
-		InputStream is = this.getClass().getResourceAsStream(zip);
-		ZipInputStream zis = new ZipInputStream(is);
-		ZipEntry entry = zis.getNextEntry();
-		entry = zis.getNextEntry();
+    private void extractZip(String zip) throws IOException
+    {
+        InputStream is = this.getClass().getResourceAsStream(zip);
+        ZipInputStream zis = new ZipInputStream(is);
+        ZipEntry entry = zis.getNextEntry();
+        entry = zis.getNextEntry();
 
-		String destination = "./temp/";
-		byte[] buf = new byte[1024];
-		while (entry != null)
-		{
-		    // do something with the entry - for example, extract the data
-			String name = entry.getName();
+        String destination = "./temp/";
+        byte[] buf = new byte[1024];
+        while (entry != null)
+        {
+            String name = entry.getName();
 
-			int n;
+            int n;
             FileOutputStream fileoutputstream;
 
             String path = destination + name;
             File newFile = new File(path);
             newFile.getParentFile().mkdirs();
-            if(name.charAt(name.length() - 1)=='/')
+            if (name.charAt(name.length() - 1) == '/')
             {
-            	zis.closeEntry();
+                zis.closeEntry();
                 entry = zis.getNextEntry();
                 continue;
             }
 
-            if(!newFile.exists()) newFile.createNewFile();
+            if (!newFile.exists())
+            {
+                newFile.createNewFile();
+            }
 
             fileoutputstream = new FileOutputStream(path);
 
@@ -72,77 +72,81 @@ public class Injector
             fileoutputstream.close();
             zis.closeEntry();
             entry = zis.getNextEntry();
-		}
-	}
+        }
+    }
 
-	public void inject (boolean bNew) throws IOException, InterruptedException, URISyntaxException
-	{
-		extractZip("Universal-Inject-Generator-master.zip");
+    public void inject(boolean bNew) throws IOException, InterruptedException, URISyntaxException
+    {
+        extractZip("Universal-Inject-Generator-master.zip");
 
-		//first, copy hs.app from files9 folder to input folder of inject generator
-		copyFile("./files9/hs.app", "./temp/Universal-Inject-Generator-master/input/hs.app");
+        // first, copy hs.app from files9 folder to input folder of inject
+        // generator
+        copyFile("./files9/hs.app", "./temp/Universal-Inject-Generator-master/input/hs.app");
 
-		//second, copy the right fbi.cia into the input folder
-		String folder = "old-FBI";
+        // second, copy the right fbi.cia into the input folder
+        String folder = "old-FBI";
 
-		if(bNew)
-		{
-			folder = "new-FBI";
-		}
-		copyFile("./temp/Universal-Inject-Generator-master/"+folder+"/fbi.cia", "./temp/Universal-Inject-Generator-master/input/fbi.cia");
+        if (bNew)
+        {
+            folder = "new-FBI";
+        }
+        copyFile("./temp/Universal-Inject-Generator-master/" + folder + "/fbi.cia",
+                "./temp/Universal-Inject-Generator-master/input/fbi.cia");
 
-		//third, run go.bat
-		Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", "go.bat"}, null, new File("./temp/Universal-Inject-Generator-master/"));
+        // third, run go.bat
+        Runtime.getRuntime().exec(new String[] { "cmd", "/c", "start", "go.bat" }, null,
+                new File("./temp/Universal-Inject-Generator-master/"));
 
-		File f = new File("./temp/Universal-Inject-Generator-master/fbi_inject_with_banner.app");
-		int counter = 0;
-		while(true) //wait till file exists before trying to move it
-		{
-			counter++;
-			if(f.exists() || counter > 200000)
-			{
-				break;
-			}
-		}
+        File f = new File("./temp/Universal-Inject-Generator-master/fbi_inject_with_banner.app");
+        int counter = 0;
+        while (true) // wait till file exists before trying to move it
+        {
+            counter++;
+            if (f.exists() || counter > 200000)
+            {
+                break;
+            }
+        }
 
-		//kill cmd window so we can delete temp files
-		Runtime.getRuntime().exec("taskkill /f /im cmd.exe") ;
+        // kill cmd window so we can delete temp files
+        Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
 
-		//give it a chance to clos
-		TimeUnit.SECONDS.sleep(1);
+        // give it a chance to close
+        TimeUnit.SECONDS.sleep(1);
 
-		//fourth, copy the new fbi app to files9 folder
-		copyFile("./temp/Universal-Inject-Generator-master/fbi_inject_with_banner.app", "./files9/fbi_inject_with_banner.app");
+        // fourth, copy the new fbi app to files9 folder
+        copyFile("./temp/Universal-Inject-Generator-master/fbi_inject_with_banner.app",
+                "./files9/fbi_inject_with_banner.app");
 
-		//fifth, delete the temp folder
-		deleteDirectory(new File("./temp/"));
-	}
+        // fifth, delete the temp folder
+        deleteDirectory(new File("./temp/"));
+    }
 
-	//the following function is taken directly from user oyo on StackExchange
-	//http://stackoverflow.com/questions/3775694/deleting-folder-from-java
-	private boolean deleteDirectory(File directory)
-	{
-	    if(directory.exists())
-	    {
-	        File[] files = directory.listFiles();
-	        if(null != files)
-	        {
-	            for(int i = 0; i < files.length; i++)
-	            {
-	                if(files[i].isDirectory())
-	                {
-	                    deleteDirectory(files[i]);
-	                    System.out.println("deleting " + files[i].getName());
-	                }
-	                else
-	                {
-	                    files[i].delete();
-	                    System.out.println("deleting " + files[i].getName());
-	                }
-	            }
-	        }
-	    }
-	    return(directory.delete());
-	}
+    // the following function is taken directly from user oyo on StackExchange
+    // http://stackoverflow.com/questions/3775694/deleting-folder-from-java
+    private boolean deleteDirectory(File directory)
+    {
+        if (directory.exists())
+        {
+            File[] files = directory.listFiles();
+            if (null != files)
+            {
+                for (int i = 0; i < files.length; i++)
+                {
+                    if (files[i].isDirectory())
+                    {
+                        deleteDirectory(files[i]);
+                        System.out.println("deleting " + files[i].getName());
+                    }
+                    else
+                    {
+                        files[i].delete();
+                        System.out.println("deleting " + files[i].getName());
+                    }
+                }
+            }
+        }
+        return (directory.delete());
+    }
 
 }
--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -17,81 +17,81 @@ import javax.swing.JRadioButton;
 
 public class MainWindow extends JPanel implements ActionListener
 {
-	private static final long serialVersionUID = -371374960795143493L;
-	private boolean bNew = false;
-	private boolean bError = false;
-	private boolean bSuccess = false;
-	private JLabel errorLabel;
-	private JButton btnGo;
-	private JRadioButton btnNew;
-	private JRadioButton btnOld;
-	private JLabel descLabel;
+    private static final long serialVersionUID = -371374960795143493L;
+    private boolean bNew = false;
+    private boolean bError = false;
+    private boolean bSuccess = false;
+    private JLabel errorLabel;
+    private JButton btnGo;
+    private JRadioButton btnNew;
+    private JRadioButton btnOld;
+    private JLabel descLabel;
 
-	public void initialise()
-	{
-		descLabel = new JLabel("Choose which FBI you want and press Go.");
-		Font labelFont = descLabel.getFont();
-		descLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 24));
-		descLabel.setMaximumSize(new Dimension(500,30));
-		descLabel.setMinimumSize(new Dimension(500,30));
-		add(descLabel);
+    public void initialise()
+    {
+        descLabel = new JLabel("Choose which FBI you want and press Go.");
+        Font labelFont = descLabel.getFont();
+        descLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 24));
+        descLabel.setMaximumSize(new Dimension(500, 30));
+        descLabel.setMinimumSize(new Dimension(500, 30));
+        add(descLabel);
 
-		JLabel blankLabel1 = new JLabel("   ");
-		blankLabel1.setMinimumSize(new Dimension(30,30));
-		add(blankLabel1);
+        JLabel blankLabel1 = new JLabel("   ");
+        blankLabel1.setMinimumSize(new Dimension(30, 30));
+        add(blankLabel1);
 
-		btnOld = new JRadioButton("Old (stable) FBI");
-		btnOld.setActionCommand("old");
-		btnOld.addActionListener(this);
-		labelFont = btnOld.getFont();
-		btnOld.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
-		btnOld.setMaximumSize(new Dimension(500,20));
-		btnOld.setMinimumSize(new Dimension(500,20));
-		add(btnOld);
-		JLabel blankLabel2 = new JLabel("   ");
-		blankLabel2.setMinimumSize(new Dimension(20,20));
-		add(blankLabel2);
+        btnOld = new JRadioButton("Old (stable) FBI");
+        btnOld.setActionCommand("old");
+        btnOld.addActionListener(this);
+        labelFont = btnOld.getFont();
+        btnOld.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
+        btnOld.setMaximumSize(new Dimension(500, 20));
+        btnOld.setMinimumSize(new Dimension(500, 20));
+        add(btnOld);
+        JLabel blankLabel2 = new JLabel("   ");
+        blankLabel2.setMinimumSize(new Dimension(20, 20));
+        add(blankLabel2);
 
-		btnNew = new JRadioButton("Newest FBI");
-		btnNew.setActionCommand("new");
-		btnNew.addActionListener(this);
-		labelFont = btnNew.getFont();
-		btnNew.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
-		btnNew.setMaximumSize(new Dimension(500,20));
-		btnNew.setMinimumSize(new Dimension(500,20));
-		add(btnNew);
+        btnNew = new JRadioButton("Newest FBI");
+        btnNew.setActionCommand("new");
+        btnNew.addActionListener(this);
+        labelFont = btnNew.getFont();
+        btnNew.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
+        btnNew.setMaximumSize(new Dimension(500, 20));
+        btnNew.setMinimumSize(new Dimension(500, 20));
+        add(btnNew);
 
-		ButtonGroup group = new ButtonGroup();
-	    group.add(btnOld);
-	    group.add(btnNew);
+        ButtonGroup group = new ButtonGroup();
+        group.add(btnOld);
+        group.add(btnNew);
 
+        JLabel blankLabel3 = new JLabel("   ");
+        blankLabel3.setMinimumSize(new Dimension(30, 30));
+        add(blankLabel3);
 
-		JLabel blankLabel3 = new JLabel("   ");
-		blankLabel3.setMinimumSize(new Dimension(30,30));
-		add(blankLabel3);
+        btnGo = new JButton("Go");
+        btnGo.setActionCommand("go");
+        btnGo.addActionListener(this);
+        btnGo.setMaximumSize(new Dimension(100, 40));
+        btnGo.setMinimumSize(new Dimension(100, 40));
+        add(btnGo);
 
-		btnGo = new JButton("Go");
-		btnGo.setActionCommand("go");
-		btnGo.addActionListener(this);
-		btnGo.setMaximumSize(new Dimension(100,40));
-		btnGo.setMinimumSize(new Dimension(100,40));
-	    add(btnGo);
+        errorLabel = new JLabel();
+        labelFont = errorLabel.getFont();
+        errorLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 16));
+        add(errorLabel);
+    }
 
-	    errorLabel = new JLabel();
-	    labelFont = errorLabel.getFont();
-	    errorLabel.setFont(new Font(labelFont.getName(), Font.PLAIN, 16));
-		add(errorLabel);
-	}
-	public MainWindow ()
-	{
-		initialise();
-	}
+    public MainWindow()
+    {
+        initialise();
+    }
 
-	private static void createWindow()
-	{
+    private static void createWindow()
+    {
         JFrame frame = new JFrame("MainWindow");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500,500));
+        frame.setPreferredSize(new Dimension(500, 500));
 
         MainWindow newContentPane = new MainWindow();
         newContentPane.setLayout(new BoxLayout(newContentPane, BoxLayout.PAGE_AXIS));
@@ -102,50 +102,52 @@ public class MainWindow extends JPanel implements ActionListener
         frame.pack();
         frame.setVisible(true);
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         createWindow();
     }
 
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-	    if ("go".equals(e.getActionCommand()))
-	    {
-	    	try
-			{
-				bError = false;
-				bSuccess = false;
-				(new Injector()).inject(bNew);
-				bSuccess = true;
-			}
-			catch (Exception io)
-			{
-				io.printStackTrace();
-				bError = true;
-			}
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if ("go".equals(e.getActionCommand()))
+        {
+            try
+            {
+                bError = false;
+                bSuccess = false;
+                (new Injector()).inject(bNew);
+                bSuccess = true;
+            }
+            catch (Exception io)
+            {
+                io.printStackTrace();
+                bError = true;
+            }
 
-	    	if(bError)
-	    	{
-	    		errorLabel.setForeground(Color.RED);
-				errorLabel.setText("Error: check files are in the right place. Did you dump your hs.app?");
-	    	}
-	    	else
-	    	{
-	    		errorLabel.setText("");
-	    	}
-	    	if(bSuccess)
-	    	{
-	    		errorLabel.setForeground(Color.GREEN);
-	    		errorLabel.setText("Success! You can put your SD card back and inject FBI now.");
-	    	}
-	    }
-	    if("new".equals(e.getActionCommand()))
-	    {
-	    	bNew = true;
-	    }
-	    if("old".equals(e.getActionCommand()))
-	    {
-	    	bNew = false;
-	    }
-	}
+            if (bError)
+            {
+                errorLabel.setForeground(Color.RED);
+                errorLabel.setText("Error: check files are in the right place. Did you dump your hs.app?");
+            }
+            else
+            {
+                errorLabel.setText("");
+            }
+            if (bSuccess)
+            {
+                errorLabel.setForeground(Color.GREEN);
+                errorLabel.setText("Success! You can put your SD card back and inject FBI now.");
+            }
+        }
+        if ("new".equals(e.getActionCommand()))
+        {
+            bNew = true;
+        }
+        if ("old".equals(e.getActionCommand()))
+        {
+            bNew = false;
+        }
+    }
 }
